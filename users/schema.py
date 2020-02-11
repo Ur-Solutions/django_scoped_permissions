6483e6807db5ffde17917621ca9fe7060d355313
Magnus Buvarp <magnus.buvarp@gmail.com>
2020-02-11 11:45:01 +0100

    Add ToggleScopedPermissionMutation

--- users/schema.py
+++ users/schema.py
@@ -1,3 +1,4 @@
+from graphql import GraphQLError
 import graphene
 from graphene import Node
 from graphene_django import DjangoObjectType, DjangoConnectionField
@@ -7,7 +8,10 @@ from graphene_django_cud.mutations import (
     DjangoUpdateMutation,
     DjangoDeleteMutation,
 )
+from graphene_django_cud.util import disambiguate_id
+from django.core.exceptions import PermissionDenied
 
+from permissions.models import ScopedPermission
 from users.models import User, UserType
 
 #################################
@@ -60,7 +64,7 @@ class UserQuery(graphene.ObjectType):
     all_user_types = DjangoConnectionField(UserTypeNode)
 
     def resolve_all_user_types(self, info, *args, **kwargs):
-        return UserTypes.objects.filter(company=info.context.company)
+        return UserType.objects.filter(company=info.context.company)
 
 
 #################################
@@ -111,6 +115,54 @@ class DeleteUserTypeMutation(DjangoDeleteMutation):
         model = UserType
 
 
+class ToggleScopedPermissionMutation(graphene.Mutation):
+    class Arguments:
+        user_type = graphene.ID(required=True)
+        scope = graphene.String(required=True)
+        exclude = graphene.Boolean(required=False)
+        remove = graphene.Boolean(required=False)
+
+    user_type = graphene.Field(UserTypeNode)
+
+    def mutate(self, info, user_type, scope, exclude=None, remove=False):
+        try:
+            user_type = UserType.objects.get(pk=disambiguate_id(user_type))
+        except UserType.DoesNotExist:
+            raise GraphQLError("UserType does not exist")
+
+        user = info.context.user
+        if not user_type.has_permission(user, "update"):
+            raise PermissionDenied("You do not have permission to do this")
+
+        if remove:
+            try:
+                if exclude is None:
+                    permission = user_type.scoped_permissions.get(scope=scope)
+                else:
+                    permission = user_type.scoped_permissions.get(
+                        scope=scope, exclude=exclude
+                    )
+                user_type.scoped_permissions.remove(permission)
+            except ScopedPermission.DoesNotExist:
+                pass
+        else:
+            if exclude is None:
+                exclude = False
+
+            # First, remove any existing permissions with same scope from user type
+            existing_permissions = user_type.scoped_permissions.filter(scope=scope)
+            if existing_permissions.exists():
+                user_type.scoped_permissions.remove(*existing_permissions)
+
+            # Then, get or create a new permission, and add it to user_type
+            permission, _ = ScopedPermission.objects.get_or_create(
+                scope=scope, exclude=exclude
+            )
+            user_type.scoped_permissions.add(permission)
+
+        return ToggleScopedPermissionMutation(user_type=user_type)
+
+
 class UserMutations(graphene.ObjectType):
     create_user = CreateUserMutation.Field()
     update_user = UpdateUserMutation.Field()
@@ -121,3 +173,5 @@ class UserMutations(graphene.ObjectType):
     update_user_type = UpdateUserTypeMutation.Field()
     patch_user_type = PatchUserTypeMutation.Field()
     delete_user_type = DeleteUserTypeMutation.Field()
+
+    toggle_scoped_permission = ToggleScopedPermissionMutation.Field()
