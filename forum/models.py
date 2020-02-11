087a11c2f4a4f2b945d50b3849539eaa545e4e36
Magnus Buvarp <magnus.buvarp@gmail.com>
2020-02-11 10:27:45 +0100

    Create model Post

--- forum/models.py
+++ forum/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from permissions.models import ScopedModelMixin
 from permissions.util import create_scope
+from users.models import User
 
 
 class Thread(ScopedModelMixin, models.Model):
@@ -12,19 +13,15 @@ class Thread(ScopedModelMixin, models.Model):
     text = models.TextField()
 
     created_by = models.ForeignKey(
-        "users.User",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=False,
-        related_name="threads",
+        User, on_delete=models.SET_NULL, null=True, blank=False, related_name="threads",
     )
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def get_base_scopes(self):
         return [
-            create_scope("thread", self.id),
-            create_scope("user", self.created_by.id, "thread", self.id),
+            create_scope(self, self.id),
+            create_scope(User, self.created_by.id, self, self.id),
         ]
 
     def __str__(self):
@@ -33,3 +30,23 @@ class Thread(ScopedModelMixin, models.Model):
         )
         return f"{self.title} by {user_name}"
 
+
+class Post(ScopedModelMixin, models.Model):
+    class Meta:
+        pass
+
+    thread = models.ForeignKey(Thread, related_name="posts", on_delete=models.CASCADE)
+
+    text = models.TextField()
+
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=False, related_name="posts",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def get_base_scopes(self):
+        return [
+            create_scope(self, self.id),
+            create_scope(User, self.created_by.id, self, self.id),
+        ]
