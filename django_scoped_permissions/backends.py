63f628234a26fad8289c83d4c3fba778ade7e8ad
Tormod Haugland <tormod.haugland@gmail.com>
2020-05-30 15:42:05 +0200

    Add ScopedAuthenticationBackend

---
+++ django_scoped_permissions/backends.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import AbstractUser
+
+from django_scoped_permissions.models import HasScopedPermissionsMixin, ScopedModelMixin
+
+
+class ScopedAuthenticationBackend(ModelBackend):
+    def has_perm(self, user_obj: AbstractUser, perm: str, obj=None):
+        if user_obj.is_anonymous:
+            return False
+
+        if user_obj.is_superuser:
+            return True
+
+        if not isinstance(user_obj, HasScopedPermissionsMixin):
+            return None
+
+        if not obj:
+            return user_obj.has_scoped_permissions(perm)
+        elif isinstance(obj, ScopedModelMixin):
+            return obj.has_permission(user_obj, perm)
+        else:
+            return None
