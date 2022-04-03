9db3378b7ab899f618d2283822c9e1077700af22
Tormod Haugland <tormod.haugland@gmail.com>
2022-04-03 09:52:00 +0200

    chore: Bumpversion 0.1.5 -> 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-scoped-permissions",
-    version="0.1.5",
+    version="0.1.6",
     author="Tormod Haugland, Magnus Buvarp",
     author_email="tormod.haugland@gmail.com, magnus.buvarp@gmail.com",
     description="Scoped permission system for Django",
