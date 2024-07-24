9dd15975599113a89ef6e1e074cb861efd0c97d8
Tormod Haugland <tormod.haugland@gmail.com>
2024-07-24 15:23:51 +0200

    Improve ScopedAuthenticationBackend
    Properly support new permission system

--- django_scoped_permissions/backends.py
+++ django_scoped_permissions/backends.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import AbstractUser
 
+from django_scoped_permissions.core.scoped_permission import ScopedPermission
 from django_scoped_permissions.models import ScopedPermissionProvider, ProtectedModelMixin
 
 
 class ScopedAuthenticationBackend(ModelBackend):
-    def has_perm(self, user_obj: AbstractUser, perm: str, obj=None):
+    def has_perm(self, user_obj: AbstractUser, perm, obj=None):
         if user_obj.is_anonymous:
             return False
 
@@ -15,9 +16,17 @@ class ScopedAuthenticationBackend(ModelBackend):
         if not isinstance(user_obj, ScopedPermissionProvider):
             return None
 
-        if not obj:
-            return user_obj.has_scoped_permissions(perm)
+        granting_permissions = user_obj.get_granting_permissions()
+
+        if obj is None:
+            required_permission = ScopedPermission.safe_create(perm, default=None)
+
+            if not required_permission:
+                return None
+
+            return required_permission.check_access(granting_permissions)
+
         elif isinstance(obj, ProtectedModelMixin):
-            return obj.has_permission(user_obj, perm)
-        else:
-            return None
+            return obj.check_access(granting_permissions)
+
+        return None
