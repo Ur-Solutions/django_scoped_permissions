326f3960121d1efe6c5b278a155fa96fc494b10f
Christian De Frene <christian@ursolutions.no>
2025-03-12 15:11:46 +0100

    Upgrade to Django v4

--- django_scoped_permissions/tests/urls.py
+++ django_scoped_permissions/tests/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
@@ -21,6 +21,6 @@ from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r"^graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path(r"^graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
